62898e5ae0d43324526af76419b197f6eed44bcc
jafar <144897803+Jafar777@users.noreply.github.com>
2024-11-09 15:23:11 +0300

    center the stories

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -58,7 +58,9 @@ export default function Home() {
       <div className='lg:mb-72 md:mb-72'>
         <CallToAction />
       </div>
+      <div className='flex items-center justify-center'>
 
+      
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7 '>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
@@ -80,7 +82,7 @@ export default function Home() {
         )}
         
       </div>
-      
+      </div>
     </div>
   );
 }
