9e44238fc2955c781a511fca3b558a54a82de644
jafar <144897803+Jafar777@users.noreply.github.com>
2024-11-09 15:41:38 +0300

    widthfix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -58,10 +58,8 @@ export default function Home() {
       <div className='lg:mb-72 md:mb-72'>
         <CallToAction />
       </div>
-      <div className='flex items-center justify-center'>
-
-      
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7 '>
+      <div className=' flex items-center justify-center'>
+      <div className='specialwidth mx-auto p-3 flex flex-col gap-8 py-7 '>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>آخر القصص</h2>
